9c472da3092d4585e8719983168e437b1b2b8c55
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-24 19:59:25 +0300

    implementing the privious code

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Contains a Flask web application API."""
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Teardown method that calls storage.close()"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
