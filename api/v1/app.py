979d5b4cd556145b697d2294d1e30a6cfd0bfe1b
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-24 20:30:21 +0300

    implementing the privious code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Contains a Flask web application API."""
-from flask import Flask, jsonify, make_response
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-import os
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Teardown method that calls storage.close()"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handler for 404 errors"""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
