7d76ecfab8df0f523b79a2a88df55f8f565cf670
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-25 14:56:44 +0300

    implementing the privious code

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,11 +4,13 @@ from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -18,7 +20,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
@@ -27,6 +31,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({})
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -42,6 +47,7 @@ def create_state():
     storage.save()
 
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
