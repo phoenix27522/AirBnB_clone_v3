2fb94cb72b7b862b8a3ad7fd93285913cac73167
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-25 15:50:50 +0300

    implementing the privious code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
-import os
+"""blue print to my Flask instance app"""
+
+
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-
+from flask import Flask, Blueprint, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
+def clear(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def page_not_found(e):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(host = '0.0.0.0', port = 5000,
+            threaded = True)
