8eab9e83ea27f8bf29272941ba369588927f4f24
Nahom kiflu <kiflunahom1994@gmail.com>
2024-02-25 19:03:24 +0300

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-# api/v1/app.py
-from flask import Flask
+import os
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-
-# Allow CORS for all routes on the API (/*) for 0.0.0.0
-CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Remove the current SQLAlchemy Session"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
